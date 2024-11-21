6793ef2df9385752e9e68a99ae8f76811be637a1
kavan266 <kavan266@gmail.com>
2024-11-21 07:31:36 -0700

    host='0.0.0.0', port=10000, debug=True

--- src/app.py
+++ src/app.py
@@ -271,4 +271,4 @@ def update_figure(status, type_selection, geography, view_mode, year_range, capa
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=10000, debug=True)
